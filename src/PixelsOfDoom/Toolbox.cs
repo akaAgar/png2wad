43d8b0d66a2d8e987889ee0c7b11b22552d884b0
AgarCdF <ambroise.garel@gmail.com>
2020-09-15 12:09:09 +0200

    Added toolbox static class

---
+++ src/PixelsOfDoom/Toolbox.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace PixelsOfDoom
+{
+    public static class Toolbox
+    {
+        public static bool IsSameRGB(this Color color, Color other)
+        {
+            return (color.R == other.R) && (color.G == other.G) && (color.B == other.B);
+        }
+    }
+}
