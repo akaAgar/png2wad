95732e37d89f0a44f8d6336cc7fb77afdcbfcdfe
AgarCdF <ambroise.garel@gmail.com>
2020-09-15 14:15:38 +0200

    Added Point.Add and Point.Mult extensions

--- src/PixelsOfDoom/Toolbox.cs
+++ src/PixelsOfDoom/Toolbox.cs
@@ -8,5 +8,15 @@ namespace PixelsOfDoom
         {
             return (color.R == other.R) && (color.G == other.G) && (color.B == other.B);
         }
+
+        public static Point Add(this Point point, Point other)
+        {
+            return new Point(point.X + other.X, point.Y + other.Y);
+        }
+
+        public static Point Mult(this Point point, Point mutiplier)
+        {
+            return new Point(point.X * mutiplier.X, point.Y * mutiplier.Y);
+        }
     }
 }
